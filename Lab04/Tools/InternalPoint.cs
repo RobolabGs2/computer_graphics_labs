32b1f72fd95b433dbc367f34781edf83cb5a4585
SID37 <1999login@mail.ru>
2020-09-29 13:29:02 +0300

    Клик по полигону

--- Lab04/Tools/InternalPoint.cs
+++ Lab04/Tools/InternalPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,61 +23,20 @@ namespace Lab04.Tools
         {
             var debug = "";
             graphics.DrawLine(_beamPen, finish.ToPointF(), new PointF(graphics.ClipBounds.Right, (float) finish.Y));
+
             _ctx.Selected = _ctx.Polygons.Where(
                 pol =>
                 {
                     if (pol.Points.Count < 3)
                         return false;
-                    var count = pol.Edges().Where(segment =>
+                    var count = pol.Edges().Select(segment =>
                     {
-                        if (segment.P1 == segment.P2)
-                        {
-                            return false;
-                        }
-                        if (finish.Y < Math.Min(segment.P1.Y, segment.P2.Y) ||
-                            finish.Y > Math.Max(segment.P1.Y, segment.P2.Y) ||
-                            finish.X > Math.Max(segment.P1.X, segment.P2.X))
-                        {
-                            return false;
-                        }
-
-                        if (finish.X < Math.Min(segment.P1.X, segment.P2.X))
-                        {
-                            return true;
-                        }
-
-                        if (segment.P1.Y > segment.P2.Y)
-                        {
-                            segment = new LineSegment(segment.P2, segment.P1);
-                        }
-
-                        if (Math.Abs(finish.Y - segment.P1.Y) < 0.0001)
-                        {
-                            return false;
-                        }
-
-                        if (Math.Abs(finish.Y - segment.P2.Y) < 0.0001)
-                        {
-                            return true;
-                        }
-
-                        // if (segment
-                        // .Intersection(new LineSegment(finish,
-                        // new Point {X = Math.Max(segment.P1.X, segment.P2.X) + 100F, Y = finish.Y}))
-                        // .onLine)
-                        // {
-                        // return true;
-                        // }
-
-                        // return false;
-                        return segment.Sign(finish) <= 0;
-                    }).Select(seg =>
-                    {
-                        graphics.DrawLine(_debugPen, seg.P1.ToPointF(), seg.P2.ToPointF());
-                        return seg;
-                    }).Count();
-                    debug += "\n" + count.ToString();
-                    return count % 2 != 0;
+                        if (!(finish.X < Math.Max(segment.P1.X, segment.P2.X)
+                            && finish.X >= Math.Min(segment.P1.X, segment.P2.X)))
+                            return 0;
+                        return segment.Sign(finish);
+                    }).Sum();
+                    return count != 0;
                 }).ToList();
             // _ctx.Selected = _ctx.Polygons.Where(finish => finish.IsInternal(end)).ToList();
             graphics.DrawString(debug, new Font("Consolas", 10), _beamPen.Brush, finish.ToPointF());
