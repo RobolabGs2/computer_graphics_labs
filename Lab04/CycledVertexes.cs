cd7a5308d496a3ab871e6ee1976693218f1c5248
RobolabGs2 <slavikelk@yandex.ru>
2020-10-06 00:22:57 +0300

    Триангуляция вторгающейся точкой

---
+++ Lab04/CycledVertexes.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab04
+{
+    public class Vertex : IEnumerable<Point>
+    {
+        public Vertex(Point value)
+        {
+            Value = value;
+        }
+
+        public Vertex Next { get; set; }
+        public Vertex Previous { get; set; }
+        public Point Value { get; set; }
+
+        public Vertex Double()
+        {
+            return Next = Next.Previous = new Vertex(Value) {Next = Next, Previous = this};
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return new VertexPointEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class VertexPointEnumerator : IEnumerator<Point>
+    {
+        private readonly VertexEnumerator enumerator;
+
+        internal VertexPointEnumerator(Vertex start)
+        {
+            enumerator = new VertexEnumerator(start);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            return enumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            enumerator.Reset();
+        }
+
+        public Point Current => enumerator.Current.Value;
+
+        object IEnumerator.Current => Current;
+    }
+
+    public class VertexEnumerator : IEnumerator<Vertex>
+    {
+        private readonly Vertex start;
+
+        internal VertexEnumerator(Vertex start)
+        {
+            this.start = start;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (Current == null)
+            {
+                Current = start;
+                return true;
+            }
+
+            if (Current.Next == start)
+                return false;
+            Current = Current.Next;
+            return true;
+        }
+
+        public void Reset()
+        {
+            Current = start;
+        }
+
+        public Vertex Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+    }
+
+    public class CycledVertexes : IEnumerable<Vertex>
+    {
+        public Vertex V;
+
+        public CycledVertexes(Polygon origin)
+        {
+            var v = origin.Points.Select(p => new Vertex(p)).ToList();
+            V = v.First();
+            var last = v.Aggregate((v1, v2) =>
+            {
+                v2.Previous = v1;
+                v1.Next = v2;
+                return v2;
+            });
+            last.Next = V;
+            V.Previous = last;
+        }
+
+        public CycledVertexes(Vertex vertex)
+        {
+            V = vertex;
+        }
+
+        public void FindConvexVertex()
+        {
+            while (new LineSegment(V.Previous.Value, V.Value).Sign(V.Next.Value) > 0)
+            {
+                V = V.Next;
+            }
+        }
+
+        public Vertex FindIntrudingVertex()
+        {
+            var a = V.Previous;
+            var b = V;
+            var c = V.Next;
+            var triangle = new Polygon {Points = {a.Value, b.Value, c.Value}};
+            var ca = new LineSegment(c.Value, a.Value);
+            return this.Skip(2).TakeWhile(p => p != a)
+                .Where(p => triangle.IsInternal(p.Value))
+                .Aggregate((Vertex) null,
+                    (v1, v2) => v1 == null ? v2 : (ca.Distance(v1.Value) < ca.Distance(v2.Value) ? v2 : v1));
+        }
+
+        public IEnumerable<CycledVertexes> Triangulate()
+        {
+            while (V.Count() > 3)
+            {
+                FindConvexVertex();
+                var d = FindIntrudingVertex();
+                if (d == null)
+                {
+                    d = V.Next;
+                    V = V.Previous;
+                }
+
+                foreach (var r in Split(d).Triangulate())
+                {
+                    yield return r;
+                }
+            }
+
+            yield return this;
+        }
+
+        public CycledVertexes Split(Vertex by)
+        {
+            if (V == by)
+            {
+                return null;
+            }
+
+            var dv = V.Double();
+            var db = by.Double();
+            V.Next = db;
+            db.Previous = V;
+
+            dv.Previous = by;
+            by.Next = dv;
+            return new CycledVertexes(by);
+        }
+
+        public void SetCurrent(Point p)
+        {
+            V = this.First(point => p == point.Value);
+        }
+
+        public Polygon ToPolygon()
+        {
+            return new Polygon {Points = V.ToList()};
+        }
+
+
+        public IEnumerator<Vertex> GetEnumerator()
+        {
+            return new VertexEnumerator(V);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Lab04/Polygon.cs
+++ Lab04/Polygon.cs
@@ -30,22 +30,24 @@ namespace Lab04
 
             for (int i = 0; i < count; i++)
                 area += Points[i].X * Points[i + 1].Y -
-                    Points[i + 1].X * Points[i].Y;
+                        Points[i + 1].X * Points[i].Y;
             area += Points[count].X * Points[0].Y -
                     Points[0].X * Points[count].Y;
 
 
             return area / 2;
         }
+
         // Меняет направление на по-часовой при необходимости
         public void Repair()
         {
             if (Area() < 0)
                 Points.Reverse();
         }
+
         public void Apply(Matrix matrix)
         {
-            Points = Points.Select(p => p * matrix).ToList(); 
+            Points = Points.Select(p => p * matrix).ToList();
             Repair();
         }
 
@@ -69,12 +71,13 @@ namespace Lab04
                 DrawPoint(g, pen, matrix);
                 return;
             }
+
             for (int i = 1; i < Points.Count; ++i)
             {
                 Point p1 = (matrix != null) ? Points[i - 1] * matrix : Points[i - 1];
                 Point p2 = (matrix != null) ? Points[i] * matrix : Points[i];
-                g.DrawLine(pen, (float)p1.X, (float)p1.Y,
-                    (float)p2.X, (float)p2.Y);
+                g.DrawLine(pen, (float) p1.X, (float) p1.Y,
+                    (float) p2.X, (float) p2.Y);
             }
         }
 
@@ -83,9 +86,10 @@ namespace Lab04
             PartialDraw(g, pen);
             if (Points.Count > 2)
                 g.DrawLine(pen,
-                    (float)Points[Points.Count - 1].X, (float)Points[Points.Count - 1].Y,
-                    (float)Points[0].X, (float)Points[0].Y);
+                    (float) Points[Points.Count - 1].X, (float) Points[Points.Count - 1].Y,
+                    (float) Points[0].X, (float) Points[0].Y);
         }
+
         public void Draw(Graphics g, Pen pen, Matrix matrix)
         {
             PartialDraw(g, pen, matrix);
@@ -95,8 +99,8 @@ namespace Lab04
                 Point p1 = (matrix != null) ? Points[Points.Count - 1] * matrix : Points[Points.Count - 1];
                 Point p2 = (matrix != null) ? Points[0] * matrix : Points[0];
                 g.DrawLine(pen,
-                    (float)p1.X, (float)p1.Y,
-                    (float)p2.X, (float)p2.Y);
+                    (float) p1.X, (float) p1.Y,
+                    (float) p2.X, (float) p2.Y);
             }
         }
 
@@ -104,8 +108,9 @@ namespace Lab04
         {
             for (var i = 0; i < Points.Count - 1; ++i)
             {
-                yield return new LineSegment(Points[i], Points[i+1]);
+                yield return new LineSegment(Points[i], Points[i + 1]);
             }
+
             yield return new LineSegment(Points.Last(), Points.First());
         }
 
@@ -127,18 +132,38 @@ namespace Lab04
         {
             if (Points.Count < 3)
                 return false;
-            for(int i = 0; i < Points.Count; ++i)
+            for (int i = 0; i < Points.Count; ++i)
             {
                 LineSegment line = new LineSegment(Points[i], Points[(i + 1) % Points.Count]);
                 if (line.Sign(Points[(i + 2) % Points.Count]) > 0)
                     return false;
             }
+
             return true;
+        }
+
+        public List<Polygon> Split(Point p1, Point p2)
+        {
+            var cycled = new CycledVertexes(this);
+            cycled.SetCurrent(p1);
+            var second = cycled.Split(cycled.First(v => v.Value == p2));
+            if (second == null)
+            {
+                return new List<Polygon>();
+            }
+            return new List<Polygon>
+            {
+                cycled.ToPolygon(), second.ToPolygon()
+            };
+        }
+
+        public IEnumerable<Polygon> Triangulate()
+        {
+            return new CycledVertexes(this).Triangulate().Select(p=>p.ToPolygon());
         }
 
         public Polygon Intersection(Polygon other)
         {
-
             return new Polygon();
         }
     }
