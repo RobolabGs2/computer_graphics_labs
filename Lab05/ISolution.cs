4dc7a74e7c0804f292570e8c100393b2e7014ae2
RobolabGs2 <slavikelk@yandex.ru>
2020-10-07 01:26:54 +0300

    Lab05: начальная версия с заглушками

---
+++ Lab05/ISolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+
+namespace Lab05
+{
+    public interface ISolution
+    {
+        string Name { get; }
+        Control[] Controls { get; }
+    }
+
+    public abstract class AbstractSolution : ISolution
+    {
+        public string Name { get; }
+        public abstract Control[] Controls { get; }
+
+        protected AbstractSolution(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class StubSolution : AbstractSolution
+    {
+        public StubSolution(string name) : base(name)
+        {
+            Controls = new Control[]
+            {
+                new Label {Text = name, Anchor = AnchorStyles.Left | AnchorStyles.Top, AutoSize = true},
+                new Label {Text = name, Anchor = AnchorStyles.Right | AnchorStyles.Bottom, AutoSize = true},
+                new Label {Text = @"Заглушка", Anchor = AnchorStyles.Top, AutoSize = true},
+            };
+        }
+
+        public override Control[] Controls { get; }
+    }
+}
---
+++ Lab05/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab05
+{
+    public partial class MainForm : Form
+    {
+        public MainForm(IEnumerable<ISolution> solutions)
+        {
+            InitializeComponent();
+            var tabs = new TabControl
+            {
+                Size = ClientSize,
+                Dock = DockStyle.Fill,
+            };
+            tabs.TabPages.AddRange(solutions.Select((solution, i) =>
+            {
+                var tab = new TabPage($"{i+1}. {solution.Name}")
+                {
+                    BackColor = Color.White,
+                };
+                tab.Controls.AddRange(solution.Controls);
+                return tab;
+            }).ToArray());
+            Controls.Add(tabs);
+        }
+    }
+}
