5018309ce2c72745007d3dec8b0eab6be486f578
SID37 <1999login@mail.ru>
2020-10-11 00:24:47 +0300

    Сделал L-системы

---
+++ Lab05/LSystem/Parser.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab05.LSystem
+{
+    static class Parser
+    {
+        public static Turtle Parse(string[] input)
+        {
+            Dictionary<char, string> rules = new Dictionary<char, string>();
+            Dictionary<char, Action<State, Graphics>> actions = new Dictionary<char, Action<State, Graphics>>();
+
+            try
+            {
+                foreach (string s in input)
+                    ParseLine(s, rules, actions);
+            }
+            catch
+            {
+                InformationMessage("Повторное объявление правила");
+            }
+            return new Turtle(rules, actions);
+        }
+
+        static Regex rule = new Regex(@"^(\S)\s*=>\s*(\S*)\s*$");
+        static Regex move = new Regex(@"^(\S)\s*=\s*move\s*(-?\d+)\s*$");
+        static Regex moveRange = new Regex(@"^(\S)\s*=\s*move\s*(-?\d+)\s*-\s*(-?\d+)\s*$");
+        static Regex rotate = new Regex(@"^(\S)\s*=\s*rotate\s*(-?\d+)\s*$");
+        static Regex rotateRange = new Regex(@"^(\S)\s*=\s*rotate\s*(-?\d+)\s*-\s*(-?\d+)\s*$");
+        static Regex push = new Regex(@"^(\S)\s*=\s*push\s*$");
+        static Regex pop = new Regex(@"^(\S)\s*=\s*pop\s*$");
+        static Regex color = new Regex(@"^\s*(\w*)\s*$");
+        static Regex colorPair = new Regex(@"^\s*(\w*)\s*-\s*(\w*)\s*$");
+        static Regex intPair = new Regex(@"^\s*(\d+)\s*-\s*(\d+)\s*$");
+        static Regex intString = new Regex(@"^\s*\d+\s*$");
+        static Regex spaces = new Regex(@"^\s*$");
+        static Random random = new Random();
+
+        private static double RandonRange(double a, double b)
+        {
+            if (a > b)
+                (a, b) = (b, a); 
+            return random.NextDouble() * (b - a) + a;
+        }
+
+        private static Color GetColor(string s)
+        {
+            Color result = Color.FromName(s);
+            if (result.A != 0)
+                return result;
+            InformationMessage($"Неизвестный цвет: {s}");
+            return Color.Black;
+        }
+
+        public static (Color, Color) Parsecolor(string s)
+        {
+            if (color.IsMatch(s))
+            {
+                Color result = GetColor(s);
+                return (result, result);
+            } 
+            if (colorPair.IsMatch(s))
+            {
+                GroupCollection match = colorPair.Matches(s)[0].Groups;
+                return (GetColor(match[1].Value), GetColor(match[2].Value));
+            }
+            else
+                InformationMessage($"Цвет должен быть в формате:\r\n<цвет>\r\n<цвет начала>-<цвет конца>");
+            return (Color.Black, Color.Black);
+        }
+
+        public static (int, int) ParsPair(string s)
+        {
+            if (intString.IsMatch(s))
+            {
+                return (Int32.Parse(s), Int32.Parse(s));
+            }
+            if (intPair.IsMatch(s))
+            {
+                GroupCollection match = colorPair.Matches(s)[0].Groups;
+                return (Int32.Parse(match[1].Value), Int32.Parse(match[2].Value));
+            }
+            else
+                InformationMessage($"ожидолось число или диапазон: {s}");
+            return (0, 0);
+        }
+
+        private static void ParseLine(string s, Dictionary<char, string> rules, 
+            Dictionary<char, Action<State, Graphics>> actions)
+        {
+            if (rule.IsMatch(s))
+            {
+                GroupCollection match = rule.Matches(s)[0].Groups;
+                rules.Add(match[1].Value[0], match[2].Value);
+            }
+            else
+            if (move.IsMatch(s))
+            {
+                GroupCollection match = move.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) => state.Move(g, Int32.Parse(match[2].Value)));
+            }
+            else
+            if (moveRange.IsMatch(s))
+            {
+                GroupCollection match = moveRange.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) =>
+                    state.Move(g, RandonRange(Int32.Parse(match[2].Value), Int32.Parse(match[3].Value))));
+            }
+            else
+            if (rotate.IsMatch(s))
+            {
+                GroupCollection match = rotate.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) => state.Rotate(Int32.Parse(match[2].Value)));
+            }
+            else
+            if (rotateRange.IsMatch(s))
+            {
+                GroupCollection match = rotateRange.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) =>
+                    state.Rotate(RandonRange(Int32.Parse(match[2].Value), Int32.Parse(match[3].Value))));
+            }
+            else
+            if (push.IsMatch(s))
+            {
+                GroupCollection match = push.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) => state.Push());
+            }
+            else
+            if (pop.IsMatch(s))
+            {
+                GroupCollection match = pop.Matches(s)[0].Groups;
+                actions.Add(match[1].Value[0], (state, g) => state.Pop());
+            }
+            else
+            if (spaces.IsMatch(s))
+            { }
+            else
+                InformationMessage($"Строка с непонятно чем:\r\n{s}");
+        }
+
+        public static void InformationMessage(string s)
+        {
+            MessageBox.Show(s, "Окошко-всплывашка", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Lab05/LSystem/Point.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab05.LSystem
+{
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public System.Drawing.PointF ToPointF =>
+            new System.Drawing.PointF((float)X, (float)Y);
+
+        public static Point operator -(Point p)
+        {
+            return new Point {X = -p.X, Y = -p.Y};
+        }
+
+        public static Point operator /(Point p, double v)
+        {
+            return new Point { X = p.X / v, Y = p.Y / v };
+        }
+
+        public static Point operator *(Point p, double v)
+        {
+            return new Point { X = p.X * v, Y = p.Y * v };
+        }
+        public static Point operator *(double v, Point p)
+        {
+            return p * v;
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point { X = p1.X - p2.X, Y = p1.Y - p2.Y };
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point { X = p1.X + p2.X, Y = p1.Y + p2.Y };
+        }
+
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return p1.X == p2.X && p1.Y == p2.Y;
+        }
+
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public double DistanceTo(Point another)
+        {
+            var (x, y) = (X - another.X, Y - another.Y);
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public void Draw(Graphics graphics, Pen pen, float r = 2)
+        {
+            graphics.DrawEllipse(pen, (float)X - r, (float)Y - r, r * 2, r * 2);
+        }
+
+        public static (Point p1, Point p2) ABBA(IEnumerable<Point> points)
+        {
+            var (minX, minY, maxX, maxY) = points.Aggregate(
+                (double.MaxValue, double.MaxValue, double.MinValue, double.MinValue),
+                (tuple, point) => (Math.Min(tuple.Item1, point.X), Math.Min(tuple.Item2, point.Y),
+                    Math.Max(tuple.Item3, point.X), Math.Max(tuple.Item4, point.Y)));
+            return (new Point {X = minX, Y = minY}, new Point {X = maxX, Y = maxY});
+        }
+
+    }
+}
