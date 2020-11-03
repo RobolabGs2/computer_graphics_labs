272740e4f25b61b0cc30173810092407faa2736a
Murih <askerova.anastasya@gmail.com>
2020-11-03 14:01:50 +0300

    Сделан плавающий горизонт

---
+++ Lab06/Graph3D/FloatingHorizont.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab06.Base3D;
+using Lab06.Materials3D;
+using Lab06.AuxTools3D;
+
+namespace Lab06.Graph3D
+{
+    class FloatingHorizont : IDrawing
+    {
+        public Context context;
+        FastBitmap fastBitmap;
+        public Node node;
+        int width;
+        int height;
+
+        public double xStart;
+        public double xEnd;
+        public double xStep;
+        public double yStart;
+        public double yEnd;
+
+        public void Draw(Bitmap bitmap)
+        {
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Constants.backColore);
+            graphics.Dispose();
+
+            width = bitmap.Width;
+            height = bitmap.Height;
+
+            fastBitmap = new FastBitmap(bitmap, System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            DrawHorizont();
+
+            fastBitmap.Dispose();
+        }
+
+        void DrawHorizont()
+        {
+            var maxHor = new double[width];
+            var minHor = new double[width];
+            for (int i = 0; i < width; i++)
+            {
+                maxHor[i] = double.MinValue;
+                minHor[i] = double.MaxValue;
+            }
+            var dx = width / 2;
+            var dy = height / 2;
+
+            var angle = context.camera.leftAngle;
+            var dangle = context.camera.downAngle;
+            var cos = Math.Cos(angle);
+            var sin = Math.Sin(angle);
+            var dcos = Math.Cos(dangle);
+            var dsin = Math.Sin(dangle);
+            for (double i = xEnd; i > xStart; i -= xStep)
+            {
+                for (int y = -width / 2; y < width / 2; y++)
+                {
+                    var j = (y + yStart) / context.scale;
+
+                    var rotatei = cos * i - sin * j;
+                    var rotatej = sin * i + cos * j;
+                    
+                    if ((int)y + dx >= width || (int)y + dx < 0)
+                        continue;
+                    var z = dcos * rotatei + dsin * node.Get(rotatei, rotatej);
+
+                    if (minHor[(int)y + dx] > z)
+                    {
+                        minHor[(int)y + dx] = z;
+                        fastBitmap.TrySetPixel((int)y + dx, (int)(z * context.scale) + dy, Constants.borderColore);
+                    }
+
+                    if (maxHor[(int)y + dx] < z)
+                    {
+                        maxHor[(int)y + dx] = z;
+                        fastBitmap.TrySetPixel((int)y + dx, (int)(z * context.scale) + dy, Constants.textColore);
+                    }
+                       
+                }
+
+            }
+            
+        }
+    }
+}
--- Lab06/Tools3D/Render/Render.cs
+++ Lab06/Tools3D/Render/Render.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Lab06.AuxTools3D;
+using System.Drawing.Printing;
 
 namespace Lab06.Tools3D.Render
 {
@@ -24,15 +26,89 @@ namespace Lab06.Tools3D.Render
         public void Init(ToolTab tab, Context context)
         {
             this.context = context;
+            
+            Parser p = new Parser();
+            var panelGraph = new Panel
+            {
+                Dock = DockStyle.Fill,
+                Visible = false
+            };
+            var panelTable = new TableLayoutPanel
+            {
+                ColumnCount = 2,
+                RowCount = 7,
+                Dock = DockStyle.Top,
+                ForeColor = Constants.borderColore,
+                AutoSize = true,
+                CellBorderStyle = TableLayoutPanelCellBorderStyle.Single,
+                Font = new Font("Consalas", 12),
+                Visible = false
+            };
+            var floatinHor = new CheckBox
+            { Text = "Floating horizon method", ForeColor = Constants.textColore, Font = Constants.font, Dock = DockStyle.Top, AutoSize = true };
+
+            var funcText = new TextBox { Dock = DockStyle.Fill, Text = "(x * x + sin(y) *  y) / 5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "Z = " }, funcText });
+            var XStart = new TextBox { Dock = DockStyle.Fill, Text = "-5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "X от" }, XStart });
+            var XEnd = new TextBox { Dock = DockStyle.Fill, Text = "5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "X до" }, XEnd });
+            var XStep = new TextBox { Dock = DockStyle.Fill, Text = "0.5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "X Шаг" }, XStep });
+            var YStart = new TextBox { Dock = DockStyle.Fill, Text = "-5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "Y от" }, YStart });
+            var YEnd = new TextBox { Dock = DockStyle.Fill, Text = "5" };
+            panelTable.Controls.AddRange(new Control[] { new Label { Text = "Y до" }, YEnd });
+           
+            var apply = new Button
+            {
+                Text = "Apply",
+                ForeColor = Constants.textColore,
+                Font = Constants.font,
+                Dock = DockStyle.Bottom,
+                AutoSize = true,
+                Visible = false
+            };
+            apply.Click += (s, b) =>
+            {
+                try
+                {
+                   context.drawing = new FloatingHorizont { node = p.Parse(funcText.Text),
+                       xStart = double.Parse(XStart.Text),
+                       xEnd = double.Parse(XEnd.Text),
+                       xStep =  double.Parse(XStep.Text),
+                       yStart = double.Parse(YStart.Text),
+                       yEnd = double.Parse(YEnd.Text),
+                       context = context};
+                    context.Redraw();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Ошибка: \"{e.Message}\"", "Окошко-всплывашка", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }; 
+
+            floatinHor.CheckStateChanged += (sender, args) => { panelTable.Visible = floatinHor.Checked; apply.Visible = floatinHor.Checked; };
+            tab.Settings.Controls.Add(panelGraph);
+            panelGraph.Controls.Add(panelTable);
+            panelGraph.Controls.Add(floatinHor);
+            panelGraph.Controls.Add(apply);
+
             tab.AddButton(Properties.Resources.Skeleton, false).ButtonClick += b =>
-               {
-                   context.drawing = new Skeleton(context, false);
-                   context.Redraw();
-               };
-            tab.AddButton(Properties.Resources.Cube, false).ButtonClick += b =>
+            {
+                panelGraph.Visible = false;
+                context.drawing = new Skeleton(context, false);
+                context.Redraw();
+            };
+
+            var skeleton = tab.AddButton(Properties.Resources.Cube, false);
+            skeleton.ButtonClick += b =>
             {
                 context.drawing = new Skeleton(context, true);
                 context.Redraw();
+                panelGraph.Visible = true;
+                floatinHor.Checked = false;
             };
 
             var panel = new Panel
@@ -90,6 +166,8 @@ namespace Lab06.Tools3D.Render
 
             renderButton.ButtonClick += b =>
             {
+                panelGraph.Visible = false;
+                floatinHor.Checked = false;
                 panel.Visible = true;
                 context.drawing = new ZBuffer(context);
                 SetDrawing();
@@ -99,6 +177,7 @@ namespace Lab06.Tools3D.Render
             {
                 panel.Visible = false;
             };
+
         }
 
         void SetDrawing()
@@ -121,5 +200,7 @@ namespace Lab06.Tools3D.Render
             }
             context.Redraw();
         }
+
+        
     }
 }
